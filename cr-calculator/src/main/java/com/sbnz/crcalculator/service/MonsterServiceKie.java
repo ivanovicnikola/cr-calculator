99ed53c3343e8a5334be8d35355095f8cbd14036
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2022-05-02 15:52:18 +0200

    sesija sa pravilima i sablonima

--- cr-calculator/src/main/java/com/sbnz/crcalculator/service/MonsterServiceKie.java
+++ cr-calculator/src/main/java/com/sbnz/crcalculator/service/MonsterServiceKie.java
@@ -1,19 +1,21 @@
 package com.sbnz.crcalculator.service;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import org.drools.template.DataProvider;
-import org.drools.template.DataProviderCompiler;
-import org.drools.template.objects.ArrayDataProvider;
-import org.kie.api.KieBase;
+import org.drools.decisiontable.ExternalSpreadsheetCompiler;
 import org.kie.api.builder.Message;
 import org.kie.api.builder.Results;
 import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
 import org.kie.internal.utils.KieHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Service;
 
 import com.sbnz.crcalculator.facts.Monster;
@@ -21,59 +23,48 @@ import com.sbnz.crcalculator.facts.Monster;
 @Service
 public class MonsterServiceKie implements MonsterService {
 	
-	//@Autowired
-	//private KieContainer kieContainer;
+	@Autowired
+	private KieContainer kieContainer;
 	
 	@Override
 	public Monster getClassifiedMonster(Monster monster) {
-		String drl = getAbilityModifierRules();
-		KieSession ksession = createKieSessionFromDRL(drl);
-		ksession.insert(monster.getStrength());
-		ksession.insert(monster.getDexterity());
-		ksession.insert(monster.getConstitution());
-		ksession.insert(monster.getIntelligence());
-		ksession.insert(monster.getWisdom());
-		ksession.insert(monster.getCharisma());
-		ksession.fireAllRules();
-		ksession.dispose();
-		/*KieSession kieSession = kieContainer.newKieSession("ExampleSession");
-        kieSession.insert(monster);
-        kieSession.fireAllRules();
-        kieSession.dispose();*/
+		KieSession kieSession = createKieSession();
+		kieSession.insert(monster);
+		kieSession.fireAllRules();
+		kieSession.dispose();
 		return monster;
 	}
 	
-	private String getAbilityModifierRules() {
-		InputStream template = MonsterServiceKie.class.getResourceAsStream("/templates/ability-modifiers.drt");
-		
-		DataProvider dataProvider = new ArrayDataProvider(new String[][] {
-			new String[] {"0", "1", "-5"},
-			new String[] {"2", "3", "-4"},
-			new String[] {"4", "5", "-3"},
-			new String[] {"6", "7", "-2"},
-			new String[] {"8", "9", "-1"},
-			new String[] {"10", "11", "0"},
-			new String[] {"12", "13", "1"},
-			new String[] {"14", "15", "2"},
-			new String[] {"16", "17", "3"},
-			new String[] {"18", "19", "4"},
-			new String[] {"20", "21", "5"},
-			new String[] {"22", "23", "6"},
-			new String[] {"24", "25", "7"},
-			new String[] {"26", "27", "8"},
-			new String[] {"29", "30", "9"},
-			new String[] {"30", "31", "10"},
-		});
-		
-		DataProviderCompiler converter = new DataProviderCompiler();
-		String drl = converter.compile(dataProvider, template);
-		System.out.println(drl);
+	private String getStatisticsByCrRules() {
+		InputStream template = MonsterServiceKie.class.getResourceAsStream("/templates/statistics-by-cr.drl");
+		InputStream data = MonsterServiceKie.class.getResourceAsStream("/templates/statistics-by-cr.xlsx");
+		ExternalSpreadsheetCompiler converter = new ExternalSpreadsheetCompiler();
+		String drl = converter.compile(data, template, 3, 2);
 		return drl;
 	}
-
-	private KieSession createKieSessionFromDRL(String drl){
-        KieHelper kieHelper = new KieHelper();
-        kieHelper.addContent(drl, ResourceType.DRL);
+	
+	private String getAbilityModifierRules() {
+		InputStream template = MonsterServiceKie.class.getResourceAsStream("/templates/ability-modifiers.drl");
+		InputStream data = MonsterServiceKie.class.getResourceAsStream("/templates/ability-modifiers.xlsx");
+		ExternalSpreadsheetCompiler converter = new ExternalSpreadsheetCompiler();
+		String drl = converter.compile(data, template, 3, 2);
+		return drl;
+	}
+	
+	private KieSession createKieSession() {
+		KieHelper kieHelper = new KieHelper();
+		try {
+			kieHelper = getResourceFolderFiles(kieHelper);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String modifiers = getAbilityModifierRules();
+		kieHelper.addContent(modifiers, ResourceType.DRL);
+		
+		String statsByCr = getStatisticsByCrRules();
+		kieHelper.addContent(statsByCr, ResourceType.DRL);
         
         Results results = kieHelper.verify();
         
@@ -87,5 +78,16 @@ public class MonsterServiceKie implements MonsterService {
         }
         
         return kieHelper.build().newKieSession();
-    }
+	}
+	
+	private static KieHelper getResourceFolderFiles (KieHelper kieHelper) throws IOException {  
+	    ClassLoader cl =  Thread.currentThread().getContextClassLoader().getClass().getClassLoader();
+	    ResourcePatternResolver resolver = new
+	    PathMatchingResourcePatternResolver(cl);     
+	    Resource[] resources = resolver.getResources("/rules/**/*.drl") ;  
+	    for (Resource resource: resources) {
+	    	kieHelper.addResource(ResourceFactory.newFileResource(resource.getFile()), ResourceType.DRL);
+	    }
+	    return kieHelper;
+	  }
 }
